6dfb95cce1ac118106d2a7c62af8c19f13f5b4b3
Amit Garu <agaru@uwyo.edu>
2026-02-06 14:22:52 -0700

    updates

--- src/riak_kv_get_fsm.erl
+++ src/riak_kv_get_fsm.erl
@@ -467,8 +467,10 @@ waiting_vnode_r({r, VnodeResult, Idx, _ReqId},
                                                 override_vnodes = IdxList,
                                                 get_core = NewGC});
                 {Reply, UpdGetCore2} ->
+                    ?LOG_INFO("Reply ~p, UpdGetCore2 ~p", [Reply, UpdGetCore2]),
                     StateWithReply = StateData#state{get_core = UpdGetCore2},
                     NewStateData = client_reply(Reply, StateWithReply),
+                    timer:sleep(5000),
                     update_stats(Reply, NewStateData),
                     maybe_finalize(NewStateData)
             end;
