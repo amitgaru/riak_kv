e8033656161fd4e24aa9051355df2bda776a8c41
Amit Garu <agaru@uwyo.edu>
2026-03-09 15:34:06 -0600

    updates

--- src/riak_kv_app.erl
+++ src/riak_kv_app.erl
@@ -268,8 +268,8 @@ start(_Type, _StartArgs) ->
             % exometer_report:subscribe(exometer_report_tty, [riak, riak_kv, node, gets], 'count', 1000),
 
             exometer_report:add_reporter(exometer_report_custom, []),
-            exometer_report:subscribe(exometer_report_custom, [riak, riak_kv, node, gets], 'count', 100),
-            exometer_report:subscribe(exometer_report_custom, [riak, riak_kv, node, puts], 'count', 100),
+            exometer_report:subscribe(exometer_report_custom, [riak, riak_kv, node, gets], 'count', 500),
+            exometer_report:subscribe(exometer_report_custom, [riak, riak_kv, node, puts], 'count', 500),
 
             {ok, Pid};
         {error, Reason} ->
