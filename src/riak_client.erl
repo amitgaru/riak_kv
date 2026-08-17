e0d8a998b2720df58e346009f2f13e187ce1b05f
Amit Garu <amitgaru2@gmail.com>
2026-08-17 13:55:23 -0600

    updates

--- src/riak_client.erl
+++ src/riak_client.erl
@@ -61,7 +61,7 @@
 
 -compile({no_auto_import,[put/2]}).
 %% @type default_timeout() = 60000
--define(DEFAULT_TIMEOUT, 15).
+-define(DEFAULT_TIMEOUT, 20).
 -define(DEFAULT_FOLD_TIMEOUT, 3600000).
 
 %% TODO: This type needs to be better specified and validated against
